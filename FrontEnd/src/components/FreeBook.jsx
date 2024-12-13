60b226fa952dd5442415999466327cf9de97c825
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-12-13 15:42:56 +0000

    install react slick for responsive carosal

--- FrontEnd/src/components/FreeBook.jsx
+++ FrontEnd/src/components/FreeBook.jsx
@@ -1,33 +1,88 @@
 import React from 'react';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 //how to access the data from list.json file
 import list from '../../public/list.json';
 
 const FreeBook = () => {
   //filter list if the book category is free
   const filterBook = list.filter((book) => book.category === 'Free');
+  //from react-slick library to add carousal resposive
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   return (
     <>
       <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-        <h1 className="font-semibold text-xl pb-2 ">Free offered courses</h1>
-        <p>
-          This is a fascinating book that offers a wealth of knowledge and
-          insights. Perfect for anyone looking to expand their understanding and
-          skills.
-        </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {filterBook.map((book) => (
-            <div key={book.id} className="mb-4 p-4 border rounded-lg shadow-md">
-              <h2 className="text-lg font-bold">{book.title}</h2>
-              <p className="text-sm text-gray-600">{book.author}</p>
-              <p className="text-sm text-gray-600">{book.price}</p>
-              <img
-                src={book.image}
-                alt={book.title}
-                className="w-full h-auto mt-2"
-              />
+        <div>
+          <h1 className="font-semibold text-xl pb-2 ">Free offered courses</h1>
+          <p>
+            This is a fascinating book that offers a wealth of knowledge and
+            insights. Perfect for anyone looking to expand their understanding
+            and skills.
+          </p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
             </div>
-          ))}
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+            <div>
+              <h3>7</h3>
+            </div>
+            <div>
+              <h3>8</h3>
+            </div>
+          </Slider>
         </div>
       </div>
     </>
