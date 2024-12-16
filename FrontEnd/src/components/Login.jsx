bee5a23a78fb248e8df6512d0c5a03a3cba4098b
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-12-16 13:39:57 +0000

    login component setup

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -3,18 +3,61 @@ import React from 'react';
 const Login = () => {
   return (
     <>
-      <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
-          </form>
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click on ✕ button to close</p>
-        </div>
-      </dialog>
+      <div>
+        <dialog id="my_modal_3" className="modal">
+          <div className="modal-box">
+            <form method="dialog">
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+              <h3 className="font-bold text-lg">Login</h3>
+              {/* Email */}
+              <div className="mt-4 space-y-2">
+                <span>Email</span>
+                <br />
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                />
+                <br />
+
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              </div>
+              {/* password */}
+              <div className="mt-4 space-y-2">
+                <span>Password</span>
+                <br />
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  className="w-80 px-3 py-1 border rounded-md outline-none"
+                />
+                <br />
+
+                <span className="text-sm text-red-500">
+                  This field is required
+                </span>
+              </div>
+
+              {/* Button */}
+              <div className="flex justify-around mt-6">
+                <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+                  Login
+                </button>
+                <p>
+                  Not registered?
+                  <span className="underline text-blue-500 cursor-pointer">
+                    Signup
+                  </span>
+                </p>
+              </div>
+            </form>
+          </div>
+        </dialog>
+      </div>
     </>
   );
 };
