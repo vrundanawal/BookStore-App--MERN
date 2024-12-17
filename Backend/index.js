4b35f2f319871e31ef40559b0415a54655a64ea8
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-12-17 17:50:13 +0000

    connected with mondoDb compass local setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+//connect to MongoDB with hadling try catch block
+try {
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log('MongoDB connected successfully');
+} catch (error) {
+  console.log('MongoDB connection failed');
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World***!');
