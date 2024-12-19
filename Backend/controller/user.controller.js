ce6f7171e47144c8dfb8985566c9ab10cbd92be6
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-12-19 17:06:26 +0000

    updated with async await

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,16 +1,16 @@
 import User from '../model/user.model.js';
 
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
   try {
     const { fullname, email, password } = req.body;
     //check user with email exists
-    const user = User.findOne({ email });
+    const user = await User.findOne({ email });
     if (user) {
       return res.status(400).json({ message: 'User already exists' });
     }
     //create new user
     const createNewUser = new User({ fullname, email, password });
-    createNewUser.save();
+    await createNewUser.save();
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
     console.log('Error : ', error);
