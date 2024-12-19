af93d2207395cdcca5586661b0bf0e3a1d68fce7
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-12-19 17:10:18 +0000

    created the user in database with routes and functions

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,11 @@ import cors from 'cors';
 
 //import the routes and store into a variable
 import bookRoutes from './route/book.route.js';
+import userRoutes from './route/user.route.js';
 
 const app = express();
 app.use(cors());
+app.use(express.json()); //parse the incoming request with JSON payloads
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
@@ -30,6 +32,7 @@ app.get('/', (req, res) => {
 
 //use the routes
 app.use('/books', bookRoutes);
+app.use('/user', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
