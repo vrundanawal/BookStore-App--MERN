702d31c5379b85af4052d04b6225bd37533d7394
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-12-19 15:56:10 +0000

    install cors pakage ins update into index to access different ports

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 //import the routes and store into a variable
 import bookRoutes from './route/book.route.js';
 
 const app = express();
+app.use(cors());
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
