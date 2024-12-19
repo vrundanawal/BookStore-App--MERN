7bda1273aea4098b558266e4bd6c35a0ef278f40
vrundanawal12 <vrunda.nawal@cognizant.com>
2024-12-19 12:33:57 +0000

    Define a controller to create a functionality

---
+++ Backend/controller/book.controller.js
@@ -0,0 +1,11 @@
+import Book from '../model/book.model';
+
+export const getBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (error) {
+    console.log('Error : ', error);
+    res.status(500).json(error || { message: 'Something went wrong' });
+  }
+};
